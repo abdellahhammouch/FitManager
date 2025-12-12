bba324d4538d0449dec2c46fd087659c50cd01ae
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2025-12-12 19:18:50 +0100

    Creation du fichier du login

---
+++ auth_check.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Définir les informations de l'utilisateur pour toutes les pages
+$current_user = [
+    'id' => $_SESSION['user_id'],
+    'username' => $_SESSION['username'],
+    'email' => $_SESSION['email'],
+    'full_name' => $_SESSION['full_name']
+];
+?>
